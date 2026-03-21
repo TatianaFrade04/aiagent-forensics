bb413ec7592fc72959e617d7659a5068524ba267
Tatiana Frade <2024113212@my.ipleiria.pt>
2026-03-21 23:44:32 +0000

    Remove error redirection from docker rm command in start_container.ps1

--- start_container.ps1
+++ start_container.ps1
@@ -1,4 +1,4 @@
 
 docker build -t forensics-sandbox -f docker\Dockerfile .\docker\
-docker rm -f forensics_sandbox 2>$null
+docker rm -f forensics_sandbox 
 docker run -dit --name forensics_sandbox -v "${PWD}\evidence:/forensics_raw:ro" forensics-sandbox
